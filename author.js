7a553c76ad148682e566a29583f6d1cf4dba803d
Edward Stoner <edwardlstoner@gmail.com>
2019-12-22 12:19:07 -0700

    Pass tests

--- author.js
+++ author.js
@@ -1,5 +1,17 @@
-class Author {
+var Book = require('./book.js');
 
+class Author {
+  constructor(authorDetails) {
+    this.first = authorDetails.first;
+    this.last = authorDetails.last;
+    this.books = [];
+  }
+
+  write() {
+    var newBook = new Book('Isabel', 'Allende', 'The House of the Spirits', 'April 12, 1985');
+    this.books.push(newBook);
+    return newBook;
+  }
 }
 
 module.exports = Author;
